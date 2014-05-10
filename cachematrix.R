a92d86d03e23d25c1e0c7951a8747cf7af3f3e51
Lucian Comanescu <lucian@comanescu.de>
2014-05-10 18:21:19 +0200

    added implementation on the rdpeng template

--- cachematrix.R
+++ cachematrix.R
@@ -5,14 +5,38 @@
 ## Param:   a matrix that is invertible
 ## 
 ## Return:  a list containing a functions to:
-##          1. set the value of the vector
-##          2. get the value of the vector
-##          3. set the value of the mean
-##          4. get the value of the mean
+##          1. set the value of the matrix
+##          2. get the value of the matrix
+##          3. set the value of the inverse
+##          4. get the value of the inverse
 ##
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    
+    ## initialize the inverse of 'x' (cache)
+    inverse <- NULL
+    
+    ## set the matrix
+    ## as the matrix is changing, also clear the cache
+    set <- function(y) {
+        x <<- y
+        inverse <<- NULL
+    }
+    
+    # get the matrix
+    get <- function() x
+    
+    # set the inverse (cache it)
+    setinverse <- function(solve) inverse <<- solve
+    
+    # get the cached inverse
+    getinverse <- function() inverse
+    
+    # return a list with the 4 functions
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
+    
 }
 
 
@@ -29,5 +53,20 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
     
-    ## Return a matrix that is the inverse of 'x'
+    # read the cached inverse
+    inv <- x$getinverse()
+    
+    # if already calculated, just return the value from the cache (and print a message)
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    
+    # if not cached, calculate the inverse and store it in the cache
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setinverse(inv)
+
+    ## Return the matrix that is the inverse of 'x'
+    inv
 }
